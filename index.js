e06ea301cf6555e06caab894889c56dc9cdbe32c
kuttu101996 <kuttulr1996@gmail.com>
2024-02-24 02:36:20 +0530

    cors spcification

--- index.js
+++ index.js
@@ -5,7 +5,23 @@ const userRouter = require("./routes/user.router");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+const allowedOrigins = [
+  "https://cointab-employee.netlify.app",
+  "http://localost:3000",
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+//https://cointab-employee.netlify.app/
 
 app.get("/", async (req, res) => {
   return res.send("Hello from Server!");
